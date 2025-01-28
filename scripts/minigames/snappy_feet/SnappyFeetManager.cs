3e575e44fd2df9bc6b3a9ce3225c20f62b39be00
smavanat <edwin.hadass@hotmail.com>
2025-01-28 14:50:37 +0000

    Changes to cabbage and basics of snappy feet

--- scripts/minigames/snappy_feet/SnappyFeetManager.cs
+++ scripts/minigames/snappy_feet/SnappyFeetManager.cs
@@ -6,12 +6,20 @@ namespace WGJ25{
 	{
 		private const string CROC_PATH = "res://scenes/minigames/snappy_feet/crocodile.tscn";
 		private Crocodile[] croc;
+		private StaticBody2D leftShore;
+		private StaticBody2D rightShore;
 		// Called when the node enters the scene tree for the first time.
 		public override void _Ready()
-		{
+		{	
+			leftShore = GetNode<StaticBody2D>("LeftShore");
+			if(leftShore != null) leftShore.GlobalPosition = new Vector2(-16, GameManager.SCREEN_HEIGHT - 128);
+
+			rightShore = GetNode<StaticBody2D>("RightShore");
+			if(rightShore != null) rightShore.GlobalPosition = new Vector2(GameManager.SCREEN_WIDTH - 192,GameManager.SCREEN_HEIGHT - 32);
+
 			croc = new Crocodile[6];
 			for(int i = 0; i < croc.Length; i++){
-				Crocodile temp = (Crocodile)ObjectManager.SpawnObject(CROC_PATH, new Vector2(GameManager.SCREEN_WIDTH + (32*(i+1)), GameManager.SCREEN_HEIGHT - 64), this);
+				Crocodile temp = (Crocodile)ObjectManager.SpawnObject(CROC_PATH, new Vector2(128*(i+1), GameManager.SCREEN_HEIGHT - 64), this);
 				if(i % 2 == 0) temp.IsSnappy = false;
 				else temp.IsSnappy = true;
 				croc[i] = temp;
@@ -21,6 +29,7 @@ namespace WGJ25{
 		// Called every frame. 'delta' is the elapsed time since the previous frame.
 		public override void _Process(double delta)
 		{
+
 		}
 	}
 }
