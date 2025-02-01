4035d278839056c564b4df4c8caee25b54e4a841
Joel <joelluckett@icloud.com>
2025-02-01 13:44:15 +0000

    added knob

--- scripts/minigames/baking_game/Knob.cs
+++ scripts/minigames/baking_game/Knob.cs
@@ -3,26 +3,23 @@ using System;
 
 namespace WGJ25
 {
-	public partial class Knob : Area2D
+	public partial class Knob : TextureButton
 	{
-		private Sprite2D sprite;
+		private const float fifteenDegrees = Mathf.Pi / 6;
 
 		public override void _Ready()
 		{
-			sprite = GetNode<Sprite2D>("Sprite2D");
+			
 		}
 
-        public override void _Input(InputEvent @event)
-        {
-            if (@event is InputEventMouseButton eventMouseButton && Input.IsActionJustPressed("Click") && sprite.GetRect().HasPoint(ToLocal(eventMouseButton.Position)))
-			{
-				GD.Print("Clicked");
-			}
-        }
-
-        public override void _Process(double delta)
+		public override void _Process(double delta)
 		{
 			
+		}
+
+		public void OnKnobDown()
+		{
+			SetRotation(Rotation + fifteenDegrees);
 		}
 	}
 }
