3c55140ce57bc3dd54ff4c2a5957384a38fef18c
smavanat <edwin.hadass@hotmail.com>
2025-02-01 16:02:34 +0000

    Ingredient Functionality

--- scripts/minigames/mixing_game/Ladle.cs
+++ scripts/minigames/mixing_game/Ladle.cs
@@ -1,47 +1,44 @@
 using Godot;
 using System;
 
-public partial class Ladle : CharacterBody2D
-{
-	private Sprite2D sprite;
-	private bool isBeingDragged = false;
-	private bool isFalling;
-	private const int moveSpeed = 20;
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+namespace WGJ25 {
+	public partial class Ladle : CharacterBody2D
 	{
-		sprite = GetNode<Sprite2D>("Sprite2D");
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _PhysicsProcess(double delta)
-	{
-		if(isFalling){
-			var collision = MoveAndCollide(new Vector2(0, 0.5f) * moveSpeed);
-			// if(collision != null){
-			// 	isFalling = false;
-			// }
+		private Sprite2D sprite;
+		private bool isBeingDragged = false;
+		private bool isFalling;
+		private const int moveSpeed = 20;
+		// Called when the node enters the scene tree for the first time.
+		public override void _Ready()
+		{
+			sprite = GetNode<Sprite2D>("Sprite2D");
 		}
-		else if (isBeingDragged){
-			Godot.Vector2 dir = GetViewport().GetMousePosition() - this.GlobalPosition;
-			dir.Normalized();
-			Velocity = dir*moveSpeed;
-			MoveAndSlide();
-			if(Input.IsActionJustReleased("click")){
-				isBeingDragged = false;
-				isFalling = true;
+
+		// Called every frame. 'delta' is the elapsed time since the previous frame.
+		public override void _PhysicsProcess(double delta)
+		{
+			if(isFalling){
+				MoveAndCollide(new Vector2(0, 0.5f) * moveSpeed);
 			}
-			
+			else if (isBeingDragged){
+				Godot.Vector2 dir = GetViewport().GetMousePosition() - this.GlobalPosition;
+				dir.Normalized();
+				Velocity = dir*moveSpeed;
+				MoveAndSlide();
+				if(Input.IsActionJustReleased("click")){
+					isBeingDragged = false;
+					isFalling = true;
+				}	
+			}
+		}
+
+		public override void _Input(InputEvent @event)
+		{
+			if(@event is InputEventMouseMotion eventMotion && Input.IsActionPressed("click")
+				&& sprite.GetRect().HasPoint(ToLocal(eventMotion.Position))){
+					isFalling = false;
+					isBeingDragged = true;
+			}
 		}
 	}
-
-    public override void _Input(InputEvent @event)
-    {
-        if(@event is InputEventMouseMotion eventMotion && Input.IsActionPressed("click")
-			&& sprite.GetRect().HasPoint(ToLocal(eventMotion.Position))){
-				GD.Print("Being Dragged");
-				isFalling = false;
-				isBeingDragged = true;
-		}
-    }
 }
